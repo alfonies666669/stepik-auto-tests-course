4717bc97de240fb1e56a75b981bd82235457c910
alfonies666669 <50834183+alfonies666669@users.noreply.github.com>
2021-02-24 20:30:50 +0300

    3_6_9_test

---
+++ stepik/selenium3/test_3_6_9/test_items.py
@@ -0,0 +1,11 @@
+#запуск в консоле: pytest --language=es --browser_name=chrome/firefox test_items.py
+import unittest
+import pytest
+import time
+	
+def test_parametrize(browser):
+	link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+	browser.get(link)
+	time.sleep(30)
+	button = browser.find_element_by_xpath("/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/form/button")
+	assert button is not None
